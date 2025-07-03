8db09675a04324abd920692b87b4b29db01243f8
Paulo <paulohenriquefaria05@gmail.com>
2025-07-02 22:18:32 -0300

    Incluindo meu codigo 1.0

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Criação do objeto Scanner para leitura do terminal
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitação dos dados ao usuário
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpa o buffer do Enter 
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+        // Se eu utilizar o nextDouble, ao passar o valor preciso representar com virgula.
+        // Para não ocorrer erro posso utilizar o .parseDouble, como é um comando mais avançado, não utilizarei nesse desafio.
+        
+
+        // Mensagem final com concatenação dos dados
+        // Utilizei o println para não ter que ficar dando /n para quebra de linha
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // Fecha o Scanner
+        scanner.close();
     }
 }
